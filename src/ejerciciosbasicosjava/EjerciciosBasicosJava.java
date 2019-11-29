cccca557b2b677ee5f8e9deefc8655b261b99d59
Javier <Javier@DESKTOP-HMNFAVF>
2019-11-29 15:59:17 +0100

    Todos lo ejercicios de la primera página hechos

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -37,17 +37,105 @@ public class EjerciciosBasicosJava {
     }
     
     
+    public boolean muyVanidoso(int num){
+        
+     if((num%11 == 0)|| ((num-1)%11 ==0)){
+         return true;
+     }
+        return false;
+    }
+    
+    
+    public boolean contesta(boolean matinal, boolean madre, boolean dormido){
+        
+        if((matinal && !madre) || dormido){
+            return false;
+        }
+        return true;
+    }
+    
+    
+    public boolean menorPor10(int a, int b, int c){
+        
+       if(Math.abs(a-b) >= 10 || Math.abs(a-c) >=10 || Math.abs(a-c)>=10){
+           return true;
+       }
+       return false;
+    }
+    
+    
+    public boolean digitoIgual(int a, int b){
+        
+       if((a/10 == b/10) ||(a%10 == b%10) || (a/10 == b%10) || (a%10 == b/10)){
+           
+           return true;
+       }
+       
+       return false;
+    }
+    
+    
+    public boolean multiploMultiple(int a){
+        
+      if(a>0){
+          if((a%3==0 && a%5 != 0) ||(a%5==0 && a%3 != 0) ){
+              return true;
+          }
+      }
+      return false;
+    }
+    
+    
+    public boolean menos20(int a){
+        
+        if(a>0){
+            if(((a+1)%20 == 0) || ((a+2)%20 == 0)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    
+    public int loteria(int a, int b, int c){
+        
+       if(a==b && a==c && b==c){
+           if(a==2){
+               return 10;
+           }
+           return 5;
+       }
+       
+       if(b != a && c != a){
+           return 1;
+       }
+       return 0;
+    }
+    
+    
+    public int withouDoubles(int a, int b, boolean noDoubles){
+        
+        if(noDoubles && a==b){
+            return (a+b+1);
+        }
+        
+        return (a+b);
+    }
+ 
+    
     
     
     public static void main(String[] args) {
         
        EjerciciosBasicosJava ejercicios = new EjerciciosBasicosJava();
        
-        System.out.println(ejercicios.multa(60, false));
-        
-        System.out.println(ejercicios.multa(65, false));
-        
-       System.out.println(ejercicios.multa(60, true));
+        System.out.println(ejercicios.withouDoubles(2,3,true));
+        System.out.println(ejercicios.withouDoubles(3,3,true));
+        System.out.println(ejercicios.withouDoubles(3,3,false));
+        System.out.println(ejercicios.withouDoubles(2,3,false));
+        System.out.println(ejercicios.withouDoubles(5,5,true));
+        System.out.println(ejercicios.withouDoubles(6,6,true));
 
     }
     
